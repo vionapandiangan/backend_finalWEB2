8a6779f4c4d097363c7a9033139d27c58c01ff4b
Viona Pandiangan <2382020@unai.edu>
2025-05-01 04:19:24 +0700

    new update 4

--- src/main.ts
+++ src/main.ts
@@ -58,6 +58,6 @@ async function bootstrap() {
   });
 
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
