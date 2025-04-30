6c0e60c6c10bfee1223441e33a217f514cd13848
Viona Pandiangan <2382020@unai.edu>
2025-05-01 02:39:05 +0700

    cors enable

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://frontend-final-web-2-indol.vercel.app',
+    ]
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Simple Social Media API')
